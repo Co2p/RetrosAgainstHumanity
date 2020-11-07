bcaa77d138e07f35e23bcd070a33b3bdee263e2d
Gordon Cooper <go.co2p@gmail.com>
2020-11-07 01:11:26 +0100

    add a reset button

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 import '../style/card.scss';
 import '../style/color.scss';
 import '../style/layout.scss';
-import { words } from  "../data/words.json";
-import {mainDeck} from "../data/card.json"
-import {getRandomInt, setRandomSeed} from "./util";
+import { words } from "../data/words.json";
+import { mainDeck } from "../data/card.json"
+import { getRandomInt, setRandomSeed } from "./util";
 import "./lib/seedrandom";
 import { Deck } from './deck';
 
@@ -15,14 +15,11 @@ const params = new URLSearchParams(location.search);
 const seed = params.get("cardStack");
 const draw = params.get("draw");
 
-if(seed !== null) {
+if (seed !== null) {
     setRandomSeed();
 }
 else {
-    const word = `${words[getRandomInt(words.length)]}${words[getRandomInt(words.length)]}`;
-    params.set("cardStack", word)
-    setUrlParams(params);
-    setRandomSeed();
+    newCardDeck();
 }
 
 const deck = new Deck(mainDeck, true);
@@ -40,5 +37,19 @@ if (draw === null) {
 }
 for (let index = 0; index < draw; index++) {
     deck.getCard();
+}
+
+document.getElementById("reset").addEventListener("click", () => {
+    params.set("draw", 0)
+    setUrlParams(params);
+    newCardDeck();
+    location.reload();
+})
+
+function newCardDeck() {
+    const word = `${words[getRandomInt(words.length)]}${words[getRandomInt(words.length)]}`;
+    params.set("cardStack", word);
+    setUrlParams(params);
+    setRandomSeed();
 }
 
